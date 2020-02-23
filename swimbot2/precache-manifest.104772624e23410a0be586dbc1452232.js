ad19769734269a3c1bf92a83dd45d60ae7181468
losserfpv <pascal_knol@hotmail.com>
2020-02-23 19:18:29 +0100

    added version with feedback from device

---
+++ swimbot2/precache-manifest.104772624e23410a0be586dbc1452232.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b884f9540f4e119edcc4c5664b2449f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a70b622fad112dd603ad",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a9ed69ae9f36d27fadc5",
+    "url": "./static/js/2.856b2e49.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.856b2e49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a70b622fad112dd603ad",
+    "url": "./static/js/main.0ed0da9a.chunk.js"
+  },
+  {
+    "revision": "f39b61286813da42c09b",
+    "url": "./static/js/runtime-main.53bf2bb4.js"
+  }
+]);
